ea38c55f504b1ff4edd0f1bcb3171b6f269e98b2
Alex Toro <joaleto@yahoo.com>
2013-11-09 03:23:11 -0500

    Second chart done

--- app/views/charts/charting.blade.php
+++ app/views/charts/charting.blade.php
@@ -2,10 +2,10 @@
 @section('content')
 <script src="{{URL::to('assets/js/amcharts_3.1.1/amcharts/amcharts.js')}}" type="text/javascript"></script>
 <script src="{{URL::to('assets/js/amcharts_3.1.1/amcharts/serial.js')}}" type="text/javascript"></script>
-<div id='zone_heating_data' class='hider'>
-	
+<div id='zone_heating_data' class='hide'>
 	<?php 
-	if (isset($_GET['user'])) {
+	if (isset($_GET['user'])) //if no logged user, no chart
+	{
 		$user=$_GET['user'];
 		$preffix=''; 
 		$verifier='';
@@ -29,20 +29,60 @@
 		<?php
 	}
 		
-	?>
-		
+	?>	
 </div>
 <div class="row">
 	<div class="offset2 span7"><h1>Reheat Valve Signal vs Occupancy</h1></div>
 </div>
+<div class="row">
+	<button class="btn btn-inverse offset2 span7" id='contain_chart1'>
+		<div class=" ">
+			<div class='center' id="chartContainer" style="height:400px;width:640px;background-color:#161616"></div>
+		</div>
+	</button>
+</div>
+	
+<hr>
+<div id='zone_heating_data2' class='hide'>
+	<?php 
+	if (isset($_GET['user'])) //if no logged user, no chart
+	{
+		$user=$_GET['user'];
+		$preffix=''; 
+		$verifier='';
+		$data=$data->where('user_id','=',$user);
+		?>
+		[
+		{{-- "$data" variable comes from controller "Charts" action "getChart" --}}
 
-<button class="btn btn-inverse offset2 span7" id='contain_chart1'>
-	<div class=" ">
-		<div class='center' id="chartContainer" style="height:400px;width:640px;background-color:#161616"></div>
-	</div>
-</button>
+		@foreach($data->get() as $v)
+			@if($verifier	!=	$v->DATE_READING.' '.$v->TIME_READING)
+				{{$preffix.'{ "time":"'.$v->DATE_READING.' '.$v->TIME_READING.
+				'","Zonetemperature":'.round($v->ZT).
+				',"Outdoortemperature":'.round($v->OAT).
+				',"ReheatValveSignal":'.round($v->ZRVS*100).
+				'}'}}
 
-
+			@endif
+			<?php $preffix=','; $verifier=$v->DATE_READING.' '.$v->TIME_READING;?> 
+		@endforeach
+		]
+		<?php
+	}
+		
+	?>	
+</div>
+<div class="row">
+	<div class="offset2 span7"><h1>Reheat Valve Signal vs outdoor temperature</h1></div>
+</div>
+<div class="row">
+	<button class="btn btn-inverse offset2 span7" id='contain_chart2'>
+		<div class=" ">
+			<div class='center' id="chartContainer2" style="height:400px;width:640px;background-color:#161616"></div>
+		</div>
+	</button>
+</div>
+<hr>
 @stop
 
 @section('scripts')
--- public/assets/js/zoneheatingdata.js
+++ public/assets/js/zoneheatingdata.js
@@ -44,11 +44,6 @@ AmCharts.ready(function () {
     graph.valueField = "Zonetemperature";
     graph.title = "Temperature";
     graph.type = "smoothedLine";
-    //graph.bullet = "round";
-    //graph.bulletSize = 8;
-    //graph.bulletBorderColor = "#FFFFFF";
-    //graph.bulletBorderAlpha = 1;
-    //graph.bulletBorderThickness = 2;
     graph.lineThickness = 2;
     graph.balloonText = "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>";
     chart.addGraph(graph);
@@ -123,4 +118,128 @@ AmCharts.ready(function () {
     chart.addLegend(legend);
 
     chart.write('chartContainer');
+});
+/*
+|
+|
+|
+|
+|
+|
+|
+| SECOND CHART 
+*/
+AmCharts.ready(function () {
+    var chartData2=new Object();
+    chartData2=$('#zone_heating_data2').text();
+    chartData2=eval(chartData2);
+
+    var chart2 = new AmCharts.AmSerialChart();
+    chart2.pathToImages = "http://www.amcharts.com/lib/3/images/";
+    chart2.dataProvider = chartData2;
+    chart2.categoryField = "time";
+    //chart2.sequencedAnimation=true;
+    chart2.startEffect='bounce';
+    chart2.startDuration = 1;
+    chart2.color = "#FFFFFF";
+    //chart2.addListener('dataUpdated',zoomChart);
+
+    //AXES
+
+    var tempAxis2 = new AmCharts.ValueAxis();
+    tempAxis2.position = "left";
+    tempAxis2.title = "Temperature (F)";
+    tempAxis2.gridAlpha = 0;
+    tempAxis2.axisAlpha = 0;
+    chart2.addValueAxis(tempAxis2);
+
+    // second value axis (on the right) 
+    var PercentAxis2 = new AmCharts.ValueAxis();
+    PercentAxis2.position = "right"; // this line makes the axis to appear on the right
+    //PercentAxis2.axisColor = "#FCD202";
+    PercentAxis2.title = "Percent (%)";
+    PercentAxis2.gridAlpha = 0;
+    PercentAxis2.axisThickness = 2;
+    chart2.addValueAxis(PercentAxis2);
+
+    //temp 1
+    var graph = new AmCharts.AmGraph();
+    graph.valueAxis = tempAxis2;
+    graph.valueField = "Zonetemperature";
+    graph.title = "Zone Temperature";
+    graph.type = "smoothedLine";
+    graph.lineThickness = 2;
+    graph.balloonText = "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>";
+    chart2.addGraph(graph);
+
+    //Reheat valve signal
+    var graph = new AmCharts.AmGraph();
+    graph.valueAxis = tempAxis2;
+    graph.valueField = "Outdoortemperature";
+    graph.title = "Outdoor temperature";
+    graph.type = "smoothedLine";
+    graph.lineThickness = 2;
+    graph.balloonText = "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>";
+    chart2.addGraph(graph);
+
+
+    // column graph
+    var graphZOM = new AmCharts.AmGraph();
+    graphZOM.type = "column";
+    graphZOM.valueAxis = PercentAxis2;
+    graphZOM.title = "Reheat Valve Signal";
+    graphZOM.lineColor = "#333333";
+    graphZOM.valueField = "ReheatValveSignal";
+    graphZOM.lineAlpha = 1;
+    graphZOM.fillAlphas = 1;
+    graphZOM.dashLengthField = "dashLengthColumn";
+    graphZOM.alphaField = "alpha";
+    //graphZOM.balloonText = "<span style='font-size:13px;'>[[title]] in [[category]]:<b>[[value]]</b> [[additional]]</span>";
+    chart2.addGraph(graphZOM);
+
+
+    var categoryAxis = chart2.categoryAxis;
+    categoryAxis.autoGridCount  = false;
+    categoryAxis.axisColor = "#fff";
+    categoryAxis.gridCount = chartData2.length/2;
+    categoryAxis.gridPosition = "start";
+    categoryAxis.equalSpacing=true;
+    categoryAxis.parseDates = true; // in order char to understand dates, we should set parseDates to true
+    categoryAxis.minPeriod = "mm"; // as we have data with minute interval, we have to set "mm" here.
+
+    categoryAxis.labelRotation = 90;
+
+    graph.lineColor = "#666666";
+    
+
+    // CURSOR
+    var chartCursor = new AmCharts.ChartCursor();
+    chartCursor.cursorPosition = "mouse";
+    chart2.addChartCursor(chartCursor);
+
+    // SCROLLBAR
+    var chartScrollbar = new AmCharts.ChartScrollbar();
+    //chartScrollbar.graph = graph;
+    chartScrollbar.autoGridCount=false;
+    chartScrollbar.categoryAxis='';
+    chartScrollbar.scrollbarHeight = 40;
+    //chartScrollbar.color = "#fff";
+    chartScrollbar.backgroundAlpha=1;
+    chartScrollbar.backgroundColor= "#1A1A1A";
+    chartScrollbar.graphFillAlpha=1;
+    chartScrollbar.graphFillColor= "#444444";
+    chartScrollbar.selectedBackgroundAlpha=1;
+    chartScrollbar.selectedBackgroundColor= "#aaa";
+    chartScrollbar.selectedGraphFillAlpha=1;
+    chartScrollbar.selectedGraphFillColor= "#111";
+    chartScrollbar.autoGridCount = true;
+    chart2.addChartScrollbar(chartScrollbar);
+
+    // LEGEND
+    var legend = new AmCharts.AmLegend();
+    legend.marginLeft = 110;
+    legend.useGraphSettings = true;
+    chart2.addLegend(legend);
+
+    chart2.write('chartContainer2');
 });
