d474b8fb2c173b8018f6b4e9bb26c203162fc872
Alex Toro <joaleto@yahoo.com>
2013-11-09 02:52:06 -0500

    Cleanning navbar

--- app/views/layouts/navbar.blade.php
+++ app/views/layouts/navbar.blade.php
@@ -13,57 +13,16 @@
 					<li  class="active">
 						{{link_to($link_home,'Home')}}
 					</li>
-					<!-- <li  class="">
-						{{link_to('/charts/chapters','Chapters')}}
-					</li>
-					<li  class="">
-						{{link_to('/charts/data','Data')}}
-					</li>
-					<li  class="">
-						{{link_to('/charts/chart','Charts')}}
-					</li> -->
-					{{--unchecked users--}}
-					@if (!Auth::check())
-						
-					    <!-- 
-					    <li>
-					    	<a href="#about">Wellcome user</a>
-					    </li>
+					@if(!isset($userlink)||$userlink=='')
 						<li>
-							{{link_to('user/subscribe','if new user subscribe')}}
-						</li> -->
+							<a href="{{URL::to('charts/log')}}">
+								YOU ARE NOT LOGGED IN! (click here)
+							</a>
+						</li>
 					@endif
-					
-					
-					<!-- <li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-						<ul class="dropdown-menu">
-							<li><a href="#">Action</a></li>
-							<li><a href="#">Another action</a></li>
-							<li><a href="#">Something else here</a></li>
-							<li class="divider"></li>
-							<li class="nav-header">Nav header</li>
-							<li><a href="#">Separated link</a></li>
-							<li><a href="#">One more separated link</a></li>
-						</ul>
-					</li> -->
-					@if (Auth::check())
-					<li>
-						{{link_to('logged',"Wellcome, logged user")}}
-					</li>
-					<!-- <li>
-						<a href="#contact">Button for logged user only2</a>
-					</li> -->
-					<li>
-						{{link_to('logged/logout','Log off')}}
-					</li>
-						
-
-					@endif
-					
 				</ul>
 				<div class="pull-right nav-collapse collapse">
-					<ul class="nav"><li><a href="">Wellcome {{$user}}</a></li></ul>
+					<ul class="nav"><li><a href="{{URL::to('charts/log')}}">Change user different to {{$user}}</a></li></ul>
 					
 				</div>
 			</div><!--/.nav-collapse -->
